039d82ad009697c07c39310d4913e686702d2acc
alfazsozib <alfazsozib@gmail.com>
2025-04-04 19:59:36 +0600

    pcg clear

--- client/src/components/AdvocatePage.jsx
+++ client/src/components/AdvocatePage.jsx
@@ -1,37 +1,29 @@
 import React from 'react'
-import avatar from '../assets/images/avatar.png'
+import { cardData } from '../data/card'
+
 
 function AdvocatePage() {
   return (
     <div id='advocate' className='container mx-auto p-5'>
-        <div className='data-sec relative top-12 flex justify-between'>
-            <div className='card bg-red-400 border-1 border-[rgba(10, 10, 10, 0.50)] rounded-[8px]'>
+        <div className='data-sec relative top-12 lg:grid lg:grid-cols-3 lg:gap-5'>
+            {cardData.map((item, index) => (
+            <div className='card border-1 border-[rgba(10, 10, 10, 0.50)] rounded-[8px]'>
                 <div className='card-img'>
-                    <img src={avatar} alt="avatar"  className='w-full lg:w-[440px] lg:h-[420px]'/>
+                    <img src={item.image} alt="avatar"  className='w-full'/>
                 </div>
-                <div className='card-data'>
-                    <h1 className='text-2xl font-urbanist font-bold'>John Doe</h1>
-                    <p className='text-[#42113C]'>Advocate</p>
+                <div className='card-data p-5'>
+                    <div>
+                        <h1 className='text-2xl font-urbanist font-bold'>{item.name}</h1>
+                        <p className='text-[#42113C]'>{item.role}</p>
+                    </div>
+                    <div>
+                        <h1></h1>
+                    </div>
+                   
                 </div>
             </div>
-            <div className='card bg-red-400 border-1 border-[rgba(10, 10, 10, 0.50)] rounded-[8px]'>
-                <div className='card-img'>
-                    <img src={avatar} alt="avatar"  className='w-full lg:w-[440px] lg:h-[420px]'/>
-                </div>
-                <div className='card-data'>
-                    <h1 className='text-2xl font-urbanist font-bold'>John Doe</h1>
-                    <p className='text-[#42113C]'>Advocate</p>
-                </div>
-            </div>
-            <div className='card bg-red-400 border-1 border-[rgba(10, 10, 10, 0.50)] rounded-[8px]'>
-                <div className='card-img'>
-                    <img src={avatar} alt="avatar"  className='w-full lg:w-[440px] lg:h-[420px]'/>
-                </div>
-                <div className='card-data'>
-                    <h1 className='text-2xl font-urbanist font-bold'>John Doe</h1>
-                    <p className='text-[#42113C]'>Advocate</p>
-                </div>
-            </div>
+            ))}
+            
         </div>
     </div>
   )
---
+++ client/src/data/card.js
@@ -0,0 +1,35 @@
+import Avatar from '../assets/images/avatar.png';
+
+
+export const cardData = [
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    },
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    },
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    },
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    },
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    },
+    {
+        name: 'Advocate Arthur Melo',
+        role: 'Advocate',
+        image: Avatar,
+    }
+]
